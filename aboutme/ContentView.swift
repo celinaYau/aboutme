871f2a8a367c65bcd949136c8fa07b9e37ff0b54
Celina <celinayau28@gmail.com>
2023-08-10 13:14:50 -0400

    Initial Commit

---
+++ aboutme/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  aboutme
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var showBio = false
+    
+    var body: some View {
+            
+            
+        VStack {
+            
+            
+            
+            Text("Celina Y.")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.black)
+                .multilineTextAlignment(.leading)
+            
+            
+            Text("(Get to know me)")
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.leading)
+            
+            
+            Spacer()
+            Image("Me")
+            
+            Button( action:
+                        {showBio.toggle()})
+            {
+                Text("Click to learn more about me")
+            }
+            
+        }
+            
+            if showBio {
+                Text("This is little me")
+            }
+        
+            HStack{
+                Text("I like to draw flowers ->")
+                    .multilineTextAlignment(.center)
+                
+                Image("Drawing")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .multilineTextAlignment(.center)
+                
+            }
+        }
+
+            
+        
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aboutme/aboutmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeApp.swift
+//  aboutme
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
